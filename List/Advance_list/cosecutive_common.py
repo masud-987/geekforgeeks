579fea601cf72c54053f215bff7f872b7adf3927
masud-987 <masud.shaik987@gmail.com>
2025-05-13 12:25:32 +0530

    may-13-2025

--- List/Advance_list/cosecutive_common.py
+++ List/Advance_list/cosecutive_common.py
@@ -1,12 +1,23 @@
 #https://www.geeksforgeeks.org/python-program-to-check-if-the-list-contains-three-consecutive-common-numbers-in-python/
 #check if any three consecutive numbers are equal.
 
-a = [1,2,3,4,5,6,7,8,9,10]
-array_identical = [1,1,1,2,3]
 
-x,y,*z = zip(a, a[1:], a[2:])
-print(a, a[1:], a[2:])
-print(x,y,z)
+a = [1, 2, 2, 2, 3, 4, 5]
 
-# first_3 = array[0:3]
-# print(first_3)
+# using zip function
+for x, y, z in zip(a, a[1:], a[2:]):
+    if x == y == z:
+        print(f"x : {x}")
+        print("Found")
+        break
+else:
+    print("Not Found")
+
+
+#using for loop
+for i in range(len(a)-2):
+    if a[i] == a[i+1] == a[i+2]:
+        print(f"Found in for loop. Start:{a[i]}, End:{a[i+2]}")
+        break
+    else:
+        print(f"Not found in for loop Start:{a[i]}")
